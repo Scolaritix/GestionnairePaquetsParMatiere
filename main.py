ad289ea91701f0add633b2374d7fd4a6b806ee46
SorenDeveloppement <sorenel@yahoo.com>
2024-12-14 17:21:18 +0100

    saving gitignore

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import tkinter as tk
 import tkinter.ttk as ttk
-import customtkinter as ttk
+import customtkinter as ctk
+
+
+class App:
+    def __init__(self) -> None:
+        ...
+
+
+if __name__ == "__main__":
+    ...
