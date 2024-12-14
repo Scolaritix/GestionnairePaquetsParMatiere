b91637d002ad5fac85e862a487510e8ef219d113
SorenDeveloppement <sorenel@yahoo.com>
2024-12-14 18:05:48 +0100

    saving files

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+
+import customtkinter
 import customtkinter as ctk
 
 
-class App:
+class App(ctk.CTk):
     def __init__(self) -> None:
-        self.app : ctk.CTk = ctk.CTk()
-        self.app.geometry("400x600")
-        self.app.title("Gestionnaire de paquets pas matières")
+        super().__init__()
+        self.geometry("400x300")
+        self.title("Gestionnaire de paquets par matières")
+        self.resizable(False, False)
+
+        for i in range(3):
+            self.columnconfigure(i, weight=1)
+        for i in range(15):
+            self.rowconfigure(i, weight=1)
+
+        ctk.CTkLabel(self, text="Gestion des paquets par matière").grid(row=0, column=1)
+
+        self.subject_list: list[str] = ["NSI", "SI", "Maths", ]
+        self.subject_spinner = ctk.CTkComboBox(self, values=self.subject_list)
+        self.subject_spinner.grid(row=1, column=0)
 
 
 if __name__ == "__main__":
-    ...
+    customtkinter.set_default_color_theme("blue")
+    customtkinter.set_appearance_mode("System")
+
+    app: App = App()
+    app.mainloop()
