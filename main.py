f459fa8a5ff46c87c144924cb2e795fd0eb54ccb
SorenDeveloppement <sorenel@yahoo.com>
2024-12-14 17:27:24 +0100

    saving gitignore

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import customtkinter as ctk
 
 class App:
     def __init__(self) -> None:
-        ...
+        self.app : ctk.CTk = ctk.CTk()
+        self.app.geometry("400x600")
+        self.app.title("Gestionnaire de paquets pas matières")
 
 
 if __name__ == "__main__":
